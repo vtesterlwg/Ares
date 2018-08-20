bfeb62d06aab8baa2358fb30bfa2253e6d4f4d9e
j-rockwell <jrobertrockwell@gmail.com>
2018-08-19 22:18:24 -0700

    Prepared MemberDeathEvent

--- Factions/src/main/java/com/playares/factions/event/MemberDeathEvent.java
+++ Factions/src/main/java/com/playares/factions/event/MemberDeathEvent.java
@@ -1,4 +1,35 @@
 package com.playares.factions.event;
 
-public class MemberDeathEvent {
+import com.playares.commons.bukkit.location.PLocatable;
+import com.playares.factions.factions.PlayerFaction;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+public final class MemberDeathEvent extends PlayerEvent {
+    @Getter
+    public static final HandlerList handlerList = new HandlerList();
+
+    @Getter
+    public final PlayerFaction faction;
+
+    @Getter
+    public final PLocatable locatable;
+
+    @Getter @Setter
+    public double subtractedDTR;
+
+    public MemberDeathEvent(Player who, PlayerFaction faction, PLocatable locatable, double subtractedDTR) {
+        super(who);
+        this.faction = faction;
+        this.locatable = locatable;
+        this.subtractedDTR = subtractedDTR;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
 }
