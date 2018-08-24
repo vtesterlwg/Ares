e1bfc3fe1a0f1d0340615ca76486a3e2b68a90ec
j-rockwell <jrobertrockwell@gmail.com>
2018-08-24 13:16:59 -0700

    Added Enderpearl Timer listening

--- Factions/src/main/java/com/playares/factions/listener/PlayerTimerListener.java
+++ Factions/src/main/java/com/playares/factions/listener/PlayerTimerListener.java
@@ -1,16 +1,20 @@
 package com.playares.factions.listener;
 
+import com.playares.commons.base.util.Time;
 import com.playares.commons.bukkit.event.PlayerBigMoveEvent;
 import com.playares.factions.Factions;
 import com.playares.factions.players.FactionPlayer;
 import com.playares.factions.timers.PlayerTimer;
+import com.playares.factions.timers.cont.player.EnderpearlTimer;
 import com.playares.factions.timers.cont.player.HomeTimer;
 import lombok.Getter;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 public final class PlayerTimerListener implements Listener {
     @Getter
@@ -60,5 +64,30 @@ public final class PlayerTimerListener implements Listener {
             profile.getTimers().remove(homeTimer);
             player.sendMessage(ChatColor.RED + "Home warp cancelled");
         }
+    }
+
+    @EventHandler
+    public void onPlayerLaunchProjectile(ProjectileLaunchEvent event) {
+        if (!(event.getEntity() instanceof EnderPearl)) {
+            return;
+        }
+
+        if (!(event.getEntity().getShooter() instanceof Player)) {
+            return;
+        }
+
+        final Player player = (Player)event.getEntity().getShooter();
+        final FactionPlayer profile = plugin.getPlayerManager().getPlayer(player.getUniqueId());
+        final EnderpearlTimer pearlTimer = (EnderpearlTimer)profile.getTimer(PlayerTimer.PlayerTimerType.ENDERPEARL);
+
+        if (pearlTimer != null) {
+            player.sendMessage(ChatColor.RED + "Enderpearls are locked for " +
+                    ChatColor.RED + "" + ChatColor.BOLD + Time.convertToDecimal(pearlTimer.getRemaining()) + ChatColor.RED + "s");
+
+            event.setCancelled(true);
+            return;
+        }
+
+        profile.getTimers().add(new EnderpearlTimer(player.getUniqueId(), plugin.getFactionConfig().getTimerEnderpearl()));
     }
 }
