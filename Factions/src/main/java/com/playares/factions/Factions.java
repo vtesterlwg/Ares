7a0d55e1c260acaa754d474ffa04b4eaa009cc92
j-rockwell <jrobertrockwell@gmail.com>
2018-08-25 01:04:26 -0700

    Added Player Listener

--- Factions/src/main/java/com/playares/factions/Factions.java
+++ Factions/src/main/java/com/playares/factions/Factions.java
@@ -69,6 +69,7 @@ public final class Factions extends AresPlugin {
         registerListener(new PillarListener(this));
         registerListener(new PlayerTimerListener(this));
         registerListener(new ClaimListener(this));
+        registerListener(new PlayerListener(this));
 
         registerService(new ClassService(this));
         registerService(new CustomEventService(this));
