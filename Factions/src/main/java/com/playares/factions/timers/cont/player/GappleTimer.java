c914abf5e6124a34cafe9a1c7d75ebbc163a57c7
j-rockwell <jrobertrockwell@gmail.com>
2018-08-29 18:26:19 -0700

    Added timers

---
+++ Factions/src/main/java/com/playares/factions/timers/cont/player/GappleTimer.java
@@ -0,0 +1,25 @@
+package com.playares.factions.timers.cont.player;
+
+import com.playares.factions.timers.PlayerTimer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+
+import java.util.UUID;
+
+public final class GappleTimer extends PlayerTimer {
+    public GappleTimer(UUID owner, int seconds) {
+        super(owner, PlayerTimerType.GAPPLE, seconds);
+    }
+
+    @Override
+    public void onFinish() {
+        final Player player = Bukkit.getPlayer(owner);
+
+        if (player == null) {
+            return;
+        }
+
+        player.sendMessage(ChatColor.GREEN + "Your gapples have been unlocked");
+    }
+}
