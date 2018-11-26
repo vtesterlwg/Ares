e2f20e12b659ce369b48c998105ddeb3a85b99c1
j-rockwell <jrobertrockwell@gmail.com>
2018-11-25 23:53:02 -0800

    Updated package

--- Factions/src/main/java/com/riotmc/factions/addons/events/data/KOTHTicketSession.java
+++ Factions/src/main/java/com/riotmc/factions/addons/events/data/KOTHTicketSession.java
@@ -1,17 +1,24 @@
-package com.riotmc.factions.addons.events.type;
+package com.riotmc.factions.addons.events.data;
 
 import com.riotmc.commons.bukkit.timer.Timer;
 import com.riotmc.factions.factions.PlayerFaction;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.entity.Player;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public final class KOTHTicketSession {
     @Getter @Setter public boolean active;
     @Getter @Setter public int winCondition;
-    @Getter @Setter public boolean contested;
     @Getter public Timer timer;
     @Getter @Setter public PlayerFaction capturingFaction;
+    @Getter public Set<UUID> insidePlayers;
     @Getter public Map<PlayerFaction, Integer> tickets;
+
+    public boolean isInside(Player player) {
+        return insidePlayers.contains(player.getUniqueId());
+    }
 }
--- Factions/src/main/java/com/riotmc/factions/addons/events/data/KOTHTimerSession.java
+++ Factions/src/main/java/com/riotmc/factions/addons/events/data/KOTHTimerSession.java
@@ -1,14 +1,23 @@
-package com.riotmc.factions.addons.events.type;
+package com.riotmc.factions.addons.events.data;
 
 import com.riotmc.commons.bukkit.timer.Timer;
 import com.riotmc.factions.factions.PlayerFaction;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.entity.Player;
+
+import java.util.Set;
+import java.util.UUID;
 
 public final class KOTHTimerSession {
     @Getter @Setter public boolean active;
     @Getter @Setter public int winCondition;
     @Getter @Setter public boolean contested;
     @Getter public Timer timer;
+    @Getter public Set<UUID> insidePlayers;
     @Getter @Setter public PlayerFaction capturingFaction;
+
+    public boolean isInside(Player player) {
+        return insidePlayers.contains(player.getUniqueId());
+    }
 }
