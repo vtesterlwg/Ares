f76c03f1fabb8755f27c37ca2c9f85f0801cf90c
j-rockwell <jrobertrockwell@gmail.com>
2018-11-25 23:52:50 -0800

    Added support for PlayerEnterCapzone and PlayerLeaveCapzoneEvent

--- Factions/src/main/java/com/riotmc/factions/addons/events/EventsAddon.java
+++ Factions/src/main/java/com/riotmc/factions/addons/events/EventsAddon.java
@@ -1,15 +1,20 @@
 package com.riotmc.factions.addons.events;
 
+import com.riotmc.commons.bukkit.event.PlayerBigMoveEvent;
 import com.riotmc.factions.Factions;
 import com.riotmc.factions.addons.Addon;
+import com.riotmc.factions.addons.events.listener.CaptureRegionListener;
 import lombok.Getter;
 import org.bukkit.ChatColor;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class EventsAddon implements Addon {
     public static final String PREFIX = ChatColor.GOLD + "[" + ChatColor.YELLOW + "Events" + ChatColor.GOLD + "] " + ChatColor.RESET;
 
     @Getter public final Factions plugin;
     @Getter public boolean enabled;
+    @Getter public EventManager manager;
+    @Getter public CaptureRegionListener captureRegionListener;
 
     public EventsAddon(Factions plugin) {
         this.plugin = plugin;
@@ -22,7 +27,8 @@ public final class EventsAddon implements Addon {
 
     @Override
     public void prepare() {
-
+        this.manager = new EventManager(this);
+        this.captureRegionListener = new CaptureRegionListener(this);
     }
 
     @Override
@@ -32,6 +38,7 @@ public final class EventsAddon implements Addon {
 
     @Override
     public void stop() {
-
+        PlayerQuitEvent.getHandlerList().unregister(captureRegionListener);
+        PlayerBigMoveEvent.getHandlerList().unregister(captureRegionListener);
     }
 }
--- Factions/src/main/java/com/riotmc/factions/addons/events/event/PlayerLeaveCapzoneEvent.java
+++ Factions/src/main/java/com/riotmc/factions/addons/events/event/PlayerLeaveCapzoneEvent.java
@@ -1,6 +1,7 @@
 package com.riotmc.factions.addons.events.event;
 
 import com.riotmc.factions.addons.events.data.CaptureRegion;
+import com.riotmc.factions.addons.events.type.RiotEvent;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
@@ -11,10 +12,14 @@ public final class PlayerLeaveCapzoneEvent extends PlayerEvent {
     public static final HandlerList handlerList = new HandlerList();
 
     @Getter
+    public final RiotEvent event;
+
+    @Getter
     public final CaptureRegion capRegion;
 
-    public PlayerLeaveCapzoneEvent(Player player, CaptureRegion region) {
+    public PlayerLeaveCapzoneEvent(Player player, RiotEvent event, CaptureRegion region) {
         super(player);
+        this.event = event;
         this.capRegion = region;
     }
 
