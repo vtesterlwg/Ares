eb5627ebb4d172653fcf40b486a31525947760b0
j-rockwell <jrobertrockwell@gmail.com>
2019-08-11 13:41:15 -0700

    Fixed #33 - Timers are cancelled even when attacked by teammates

--- Factions/src/main/java/com/playares/factions/listener/PlayerTimerListener.java
+++ Factions/src/main/java/com/playares/factions/listener/PlayerTimerListener.java
@@ -12,6 +12,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.EnderPearl;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageEvent;
@@ -58,8 +59,12 @@ public final class PlayerTimerListener implements Listener {
         }
     }
 
-    @EventHandler
+    @EventHandler (priority = EventPriority.HIGHEST)
     public void onEntityDamage(EntityDamageEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
         if (!(event.getEntity() instanceof Player)) {
             return;
         }
