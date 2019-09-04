c5886f01fd9d7306fb34b12e117b0ba98ce8ac51
j-rockwell <jrobertrockwell@gmail.com>
2019-09-04 14:26:16 -0700

    Added event unregistering

--- Factions/src/main/java/com/playares/factions/addons/stats/StatsAddon.java
+++ Factions/src/main/java/com/playares/factions/addons/stats/StatsAddon.java
@@ -1,16 +1,22 @@
 package com.playares.factions.addons.stats;
 
+import com.playares.commons.bukkit.event.PlayerDamagePlayerEvent;
 import com.playares.factions.Factions;
 import com.playares.factions.addons.Addon;
+import com.playares.factions.addons.events.event.EventCaptureEvent;
 import com.playares.factions.addons.stats.command.StatsCommand;
 import com.playares.factions.addons.stats.handler.FactionStatsHandler;
 import com.playares.factions.addons.stats.handler.PlayerStatsHandler;
 import com.playares.factions.addons.stats.listener.StatisticListener;
 import com.playares.factions.addons.stats.lore.TrackableItemListener;
+import com.playares.services.playerclasses.event.ConsumeClassItemEvent;
+import com.playares.services.playerclasses.event.RogueBackstabEvent;
 import lombok.Getter;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class StatsAddon implements Addon {
     @Getter public final Factions plugin;
@@ -19,6 +25,7 @@ public final class StatsAddon implements Addon {
     @Getter public FactionStatsHandler factionHandler;
 
     private TrackableItemListener trackableListener;
+    private StatisticListener statisticListener;
 
     public StatsAddon(Factions plugin) {
         this.plugin = plugin;
@@ -44,12 +51,23 @@ public final class StatsAddon implements Addon {
 
         trackableListener = new TrackableItemListener();
         plugin.registerListener(trackableListener);
-        plugin.registerListener(new StatisticListener(this));
+
+        statisticListener = new StatisticListener(this);
+        plugin.registerListener(statisticListener);
     }
 
     @Override
     public void stop() {
         BlockBreakEvent.getHandlerList().unregister(trackableListener);
         PlayerDeathEvent.getHandlerList().unregister(trackableListener);
+
+        PlayerQuitEvent.getHandlerList().unregister(statisticListener);
+        BlockBreakEvent.getHandlerList().unregister(statisticListener);
+        EntityDeathEvent.getHandlerList().unregister(statisticListener);
+        PlayerDeathEvent.getHandlerList().unregister(statisticListener);
+        EventCaptureEvent.getHandlerList().unregister(statisticListener);
+        ConsumeClassItemEvent.getHandlerList().unregister(statisticListener);
+        PlayerDamagePlayerEvent.getHandlerList().unregister(statisticListener);
+        RogueBackstabEvent.getHandlerList().unregister(statisticListener);
     }
 }
