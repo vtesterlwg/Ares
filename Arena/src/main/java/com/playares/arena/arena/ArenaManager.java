ebb16e8b28880c02c2c0771797aee63e02db79a4
j-rockwell <jrobertrockwell@gmail.com>
2019-09-07 15:53:02 -0700

    Added arena loading from file

--- Arena/src/main/java/com/playares/arena/arena/ArenaManager.java
+++ Arena/src/main/java/com/playares/arena/arena/ArenaManager.java
@@ -5,7 +5,11 @@ import com.google.common.collect.Sets;
 import com.playares.arena.Arenas;
 import com.playares.arena.arena.builder.ArenaBuilderManager;
 import com.playares.arena.arena.data.Arena;
+import com.playares.commons.bukkit.location.PLocatable;
+import com.playares.commons.bukkit.logger.Logger;
 import lombok.Getter;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -24,7 +28,53 @@ public final class ArenaManager {
     }
 
     public void load() {
+        final YamlConfiguration config = getPlugin().getConfig("arenas");
 
+        if (config == null) {
+            Logger.error("Failed to obtain arenas.yml while loading Arenas");
+            return;
+        }
+
+        if (arenas.isEmpty()) {
+            Logger.warn("Clearing existing arenas from memory while reloading data");
+        }
+
+        for (String arenaName : config.getConfigurationSection("arenas").getKeys(false)) {
+            final String path = "arenas." + arenaName + ".";
+            final String displayName = ChatColor.translateAlternateColorCodes('&', config.getString(path + "display-name"));
+
+            final PLocatable spawnA = new PLocatable(
+                    config.getString(path + "spawnpoints.a.world"),
+                    config.getDouble(path + "spawnpoints.a.x"),
+                    config.getDouble(path + "spawnpoints.a.y"),
+                    config.getDouble(path + "spawnpoints.a.z"),
+                    (float)config.getDouble(path + "spawnpoints.a.yaw"),
+                    (float)config.getDouble(path + "spawnpoints.a.pitch")
+            );
+
+            final PLocatable spawnB = new PLocatable(
+                    config.getString(path + "spawnpoints.b.world"),
+                    config.getDouble(path + "spawnpoints.b.x"),
+                    config.getDouble(path + "spawnpoints.b.y"),
+                    config.getDouble(path + "spawnpoints.b.z"),
+                    (float)config.getDouble(path + "spawnpoints.b.yaw"),
+                    (float)config.getDouble(path + "spawnpoints.b.pitch")
+            );
+
+            final PLocatable spawnSpec = new PLocatable(
+                    config.getString(path + "spawnpoints.spec.world"),
+                    config.getDouble(path + "spawnpoints.spec.x"),
+                    config.getDouble(path + "spawnpoints.spec.y"),
+                    config.getDouble(path + "spawnpoints.spec.z"),
+                    (float)config.getDouble(path + "spawnpoints.spec.yaw"),
+                    (float)config.getDouble(path + "spawnpoints.spec.pitch")
+            );
+
+            final Arena arena = new Arena(plugin, arenaName, displayName, spawnA, spawnB, spawnSpec);
+            arenas.add(arena);
+        }
+
+        Logger.print("Loaded " + arenas.size() + " Arenas");
     }
 
     public Arena obtainArena() {
