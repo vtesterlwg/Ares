727713a268b83ac8dc96938e9e9c752b70ae9d1f
j-rockwell <jrobertrockwell@gmail.com>
2019-09-11 02:25:39 -0700

    Added method to obtain a collection of Ares Profiles matching provided filter

--- Services/Profiles/src/main/java/com/playares/services/profiles/data/AresProfileDAO.java
+++ Services/Profiles/src/main/java/com/playares/services/profiles/data/AresProfileDAO.java
@@ -1,10 +1,15 @@
 package com.playares.services.profiles.data;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 import com.playares.commons.base.connect.mongodb.MongoDB;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+
+import java.util.List;
 
 public final class AresProfileDAO {
     private static final String DB_NAME = "ares";
@@ -60,5 +65,23 @@ public final class AresProfileDAO {
         }
 
         return new AresProfile().fromDocument(existing);
+    }
+
+    public static ImmutableList<AresProfile> getProfiles(MongoDB database, Bson filter) {
+        final List<AresProfile> results = Lists.newArrayList();
+        final MongoCollection<Document> collection = database.getCollection(DB_NAME, DB_COLL);
+        final MongoCursor<Document> cursor;
+
+        if (collection == null) {
+            return ImmutableList.copyOf(results);
+        }
+
+        cursor = collection.find(filter).iterator();
+
+        while (cursor.hasNext()) {
+            results.add(new AresProfile().fromDocument(cursor.next()));
+        }
+
+        return ImmutableList.copyOf(results);
     }
 }
