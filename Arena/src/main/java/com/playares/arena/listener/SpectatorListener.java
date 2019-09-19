25c7fedfbdb2038f6cf6e1dce6494013208b307e
j-rockwell <jrobertrockwell@gmail.com>
2019-09-18 17:22:34 -0700

    Set spectators hand to null if they're spectating and launch a projectile

--- Arena/src/main/java/com/playares/arena/listener/SpectatorListener.java
+++ Arena/src/main/java/com/playares/arena/listener/SpectatorListener.java
@@ -3,16 +3,20 @@ package com.playares.arena.listener;
 import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
 import com.playares.arena.Arenas;
 import com.playares.arena.player.ArenaPlayer;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
 
-@AllArgsConstructor
 public final class SpectatorListener implements Listener {
     @Getter public final Arenas plugin;
+
+    public SpectatorListener(Arenas arenaPlugin) {
+        this.plugin = arenaPlugin;
+    }
 
     @EventHandler
     public void onProjectileLaunch(ProjectileLaunchEvent event) {
@@ -28,11 +32,15 @@ public final class SpectatorListener implements Listener {
         final ArenaPlayer profile = plugin.getPlayerManager().getPlayer(player);
 
         if (profile == null) {
+            player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+            player.getInventory().setItemInOffHand(new ItemStack(Material.AIR));
             event.setCancelled(true);
             return;
         }
 
         if (!profile.getStatus().equals(ArenaPlayer.PlayerStatus.INGAME)) {
+            player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+            player.getInventory().setItemInOffHand(new ItemStack(Material.AIR));
             event.setCancelled(true);
         }
     }
