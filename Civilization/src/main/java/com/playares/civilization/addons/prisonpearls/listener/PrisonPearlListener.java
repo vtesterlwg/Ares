888ca2a5e57bed1665d1f1464f934cfe14fc6f51
j-rockwell <jrobertrockwell@gmail.com>
2020-01-02 20:28:59 -0800

    Improved Prison Pearl event listening

--- Civilization/src/main/java/com/playares/civilization/addons/prisonpearls/listener/PrisonPearlListener.java
+++ Civilization/src/main/java/com/playares/civilization/addons/prisonpearls/listener/PrisonPearlListener.java
@@ -2,12 +2,17 @@ package com.playares.civilization.addons.prisonpearls.listener;
 
 import com.playares.civilization.addons.prisonpearls.PrisonPearlAddon;
 import com.playares.civilization.addons.prisonpearls.data.PrisonPearl;
+import com.playares.civilization.util.CivUtils;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public final class PrisonPearlListener implements Listener {
     @Getter public final PrisonPearlAddon addon;
@@ -40,7 +45,32 @@ public final class PrisonPearlListener implements Listener {
             prisonPearl.updateLocation(killer);
         }
 
-        // TODO: Send slain player to the end
+        addon.getPrisonPearlManager().getPearlRepository().add(prisonPearl);
+
+        CivUtils.sendMessage(Bukkit.getOnlinePlayers(), event.getDeathMessage(), player.getLocation(), addon.getAddonManager().getPlugin().getCivConfig().getDeathMessageRange());
+
+        CivUtils.sendMessage(
+                Bukkit.getOnlinePlayers(),
+                ChatColor.DARK_PURPLE + player.getName() + ChatColor.RED + " has been imprisoned by " + ChatColor.DARK_PURPLE + killer.getName(),
+                player.getLocation(),
+                addon.getAddonManager().getPlugin().getCivConfig().getDeathMessageRange());
+
+        event.setDeathMessage(null);
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        final Player player = event.getPlayer();
+        final PrisonPearl pearl = addon.getPrisonPearlManager().getPearl(player);
+
+        if (pearl == null) {
+            return;
+        }
+
+        final World end = Bukkit.getWorld("world_the_end");
+        event.setRespawnLocation(end.getSpawnLocation());
+
+        // You have been imprisoned by
     }
 
     @EventHandler
